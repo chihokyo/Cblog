5750a73b2371ab244d00b8a277329f196989b2fb
chihokyo <chihoukyo@gmail.com>
2019-10-13 23:09:44 +0900

    category finished

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
+from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Singup
 
+def get_category_count():
+	queryset = Post.objects.values('categories__title').annotate(Count('categories__title'))
+	return queryset
 
 def index(request):
 	featured = Post.objects.filter(featured=True)
@@ -20,7 +25,26 @@ def index(request):
 	return render(request,'index.html',content)
 
 def blog(request):
-	return render(request,'blog.html',{})
+	category_count = get_category_count()
+	most_recent = Post.objects.order_by('-timestamp')[:3]
+	post_list  = Post.objects.all()
+	paginator = Paginator(post_list,4)
+	page_request_var = 'page'
+	page = request.GET.get('page')
+	try:
+		paginated_queryset = paginator.page(page)
+	except PageNotAnInteger:
+		paginated_queryset = paginator.page(1)
+	except:
+		paginated_queryset = paginator.page(paginator.num_pages)
 
-def post(request):
+	context = {
+		'queryset':paginated_queryset,
+		'page_request_var':page_request_var,
+		'most_recent':most_recent,
+		'category_count':category_count
+	}
+	return render(request,'blog.html',context)
+
+def post(request,id):
 	return render(request,'post.html',{})
